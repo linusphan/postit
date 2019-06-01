b3b41c90ba8488ee9ff55c54b34988d6d498d875
Linus Phan <phanlinus@gmail.com>
2019-05-31 23:48:40 -0700

    default to establish connection between model and db in development; create posts table and post model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,10 @@ PostitTemplate::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Restore behavior of ActiveRecord models displaying their column information
+  # immediately after the console has loaded
+  console do
+    ActiveRecord::Base.connection
+  end
 end
