532c290ba1148c3f9795f403d5d0f6bd97fa8e8b
Linus Phan <phanlinus@gmail.com>
2019-05-31 23:59:06 -0700

    create user model and edit posts table schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_063051) do
+ActiveRecord::Schema.define(version: 2019_06_01_065624) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
