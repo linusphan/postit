7e5c6a5faa025ef95b194366fe9bd261a9568f62
Linus Phan <phanlinus@gmail.com>
2019-06-01 00:18:44 -0700

    set model layer and db between users, comments, and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_065624) do
+ActiveRecord::Schema.define(version: 2019_06_01_070839) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
