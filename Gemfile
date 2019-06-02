4806f3f54ac6cdb3cc7703a165cc0ac556a93a47
Linus Phan <phanlinus@gmail.com>
2019-06-02 14:27:54 -0700

    add authentication with Rail's has_secure_password built-in method

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 2.3.2.0'
+gem 'bcrypt-ruby'
 
 group :doc do
   gem 'sdoc', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_171829) do
+ActiveRecord::Schema.define(version: 2019_06_02_211513) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_06_01_171829) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
