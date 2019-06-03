77a7bc674a7fe5f43263e68c9320d063aec2c067
Linus Phan <phanlinus@gmail.com>
2019-06-02 22:19:41 -0700

    create Vote model; set up polymorphic associations for votes with posts and comments at the database and model layers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_211513) do
+ActiveRecord::Schema.define(version: 2019_06_03_050344) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 2019_06_02_211513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.boolean "vote"
+    t.integer "user_id"
+    t.string "voteable_type"
+    t.integer "voteable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["voteable_type", "voteable_id"], name: "index_votes_on_voteable_type_and_voteable_id"
   end
 
 end
