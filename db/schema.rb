dca510e106c2cbfb20b4d5cc4169f068d160764a
Linus Phan <phanlinus@gmail.com>
2019-07-18 16:23:57 -0700

    implement simple admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_224137) do
+ActiveRecord::Schema.define(version: 2019_07_18_230928) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_07_18_224137) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "role"
   end
 
   create_table "votes", force: :cascade do |t|
