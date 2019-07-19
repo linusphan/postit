e58202160cb15da21accc8769cb240c9fab5d9e5
Linus Phan <phanlinus@gmail.com>
2019-07-18 20:16:38 -0700

    build better slugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :votes, as: :voteable
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :description, presence: true
@@ -29,7 +29,33 @@ class Post < ActiveRecord::Base
     self.votes.where(vote: false).size
   end
 
-  def generate_slug
-    self.slug = self.title.gsub(' ', '-').downcase
+  def generate_slug!
+    the_slug = to_slug(self.title)
+    post = Post.find_by(slug: the_slug)
+    count = 2
+
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by(slug: the_slug)
+      count += 1
+    end
+
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub!(/\s*[^A-Za-z0-9]\s*/, '-')
+    str.gsub!(/-+/, '-')
+
+    str.downcase
   end
 end
