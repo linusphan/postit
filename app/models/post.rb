8dfa324c2f5523182a04543d916f184f7b87a4da
Linus Phan <phanlinus@gmail.com>
2019-07-19 18:51:01 -0700

    extract common instance methods to a module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
+  include Voteable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   before_save :generate_slug!
 
@@ -11,23 +12,11 @@ class Post < ActiveRecord::Base
   validates :description, presence: true
   validates :url, presence: true, uniqueness: true
 
-  def total_votes
-    up_votes - down_votes
-  end
-
   def to_param
     self.slug
   end
 
   private
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
 
   def generate_slug!
     the_slug = to_slug(self.title)
